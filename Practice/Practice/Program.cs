6b3835a32b4bbafeee07283f5a6f5a5034f543ea
Mahtab Hossain <63159321+MahtabTanim@users.noreply.github.com>
2024-11-24 20:19:50 +0600

    readme

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class GFG {
+    public static int Sum(){
+        int[] numbers = { 5, 8, 3, 4, 2 };
+        int sum = 0;
+        foreach(int n in numbers)
+        {
+            sum= sum+n;
+        }
+        return sum;
+    }
+    public static int VowelCount(){
+        int vowelCount = 0;
+        string name = "Write an algorithm to count the number of vowels (a, e, i, o, u) in a given string.";
+        foreach(char n in name)
+        {
+            if (n == 'a' || n == 'e' || n == 'i' || n == 'o' || n == 'u')
+            {
+                vowelCount++;
+            }
+        }
+        return vowelCount;
+    }
+    // Main Method
+    public static void Main(String[] args)
+    {
+       
+       System.Console.WriteLine(VowelCount());
+    }
+}
